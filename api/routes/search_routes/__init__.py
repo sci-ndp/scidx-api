33f881d06a2e0d02f97c87869fafeb3074d17891
rbardaji <rbardaji@gmail.com>
2024-07-01 15:49:52 +0000

    typo

--- api/routes/search_routes/__init__.py
+++ api/routes/search_routes/__init__.py
@@ -2,10 +2,10 @@ from fastapi import APIRouter
 
 from .search_datasource_route import router as get_router
 from .list_organizations_route import router as list_organizations_router
-# from .search_kafka_route import router as get_kafka
+from .search_kafka_route import router as get_kafka
 
 router = APIRouter()
 
 router.include_router(get_router)
-# router.include_router(get_kafka)
+router.include_router(get_kafka)
 router.include_router(list_organizations_router)
