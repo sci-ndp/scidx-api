2c359d4321e9662af7cd29f362435f9b62729cdd
Andreufb <andreufb97@gmail.com>
2024-08-01 11:26:05 -0600

    Enhanced Kafka stream processing: added data_key for nested data handling and info_key for adding additional information.

--- api/services/streaming_services/data_cleaning.py
+++ api/services/streaming_services/data_cleaning.py
@@ -93,6 +93,9 @@ async def process_kafka_stream(stream, filter_semantics, buffer_lock, send_data,
     kafka_port = stream.extras['port']
     kafka_topic = stream.extras['topic']
     mapping = stream.extras['mapping']
+    processing = stream.extras.get('processing', {})
+    data_key = processing.get('data_key', None)
+    info_key = processing.get('info_key', None)
     logger.info(f"Processing Kafka stream: {kafka_topic} from {kafka_host}:{kafka_port} with mapping {mapping}")
 
     consumer = AIOKafkaConsumer(
@@ -105,15 +108,21 @@ async def process_kafka_stream(stream, filter_semantics, buffer_lock, send_data,
 
     try:
         start_time = loop.time()
-        last_send_time = time.time()  # Track the last time we sent data
+        last_send_time = time.time()
         messages = []
-        # logger.info('STARTING!')
+        additional_info = None
 
         while True:
             try:
                 message = await asyncio.wait_for(consumer.getone(), timeout=TIME_WINDOW)
-                # logger.info(f"Received message: {message.value}")
                 data = json.loads(message.value)
+                
+                if info_key:
+                    additional_info = data.get(info_key, {})
+
+                if data_key:
+                    data = data.get(data_key, {})
+                
                 messages.append(data)
             except asyncio.TimeoutError:
                 logger.info("No new messages received in TIME_WINDOW")
@@ -123,22 +132,19 @@ async def process_kafka_stream(stream, filter_semantics, buffer_lock, send_data,
 
             # Check if it's time to send the accumulated messages
             if len(messages) >= CHUNK_SIZE or time_since_last_send >= TIME_WINDOW or elapsed_time >= TIME_WINDOW:
-                # logger.info(f'SENDING {len(messages)} messages')
-                await process_and_send_data(messages, mapping, stream, send_data, buffer_lock, loop, filter_semantics)
+                await process_and_send_data(messages, mapping, stream, send_data, buffer_lock, loop, filter_semantics, additional_info)
                 messages = []  # Clear the messages list after sending
                 start_time = loop.time()
                 last_send_time = time.time()  # Reset the last send time
 
             # Break the loop if no more messages are expected
             if not messages and time_since_last_send >= TIME_WINDOW:
-                # logger.info("No more messages to process, exiting loop")
                 break
 
         # Process remaining messages if any after the consumer stops
         if messages:
             # logger.info('FINAL SENDING')
-            # logger.info(f"Processing {len(messages)} remaining messages before stopping consumer")
-            await process_and_send_data(messages, mapping, stream, send_data, buffer_lock, loop, filter_semantics)
+            await process_and_send_data(messages, mapping, stream, send_data, buffer_lock, loop, filter_semantics, additional_info)
 
     except Exception as e:
         logger.error(f"Error in Kafka stream processing: {e}")
@@ -147,17 +153,21 @@ async def process_kafka_stream(stream, filter_semantics, buffer_lock, send_data,
         # Ensure consumer stops correctly
         if not consumer._closed:
             await consumer.stop()
-        # logger.info(f"Consumer stopped for Kafka stream: {kafka_topic}")
 
 
-
-async def process_and_send_data(messages, mapping, stream, send_data, buffer_lock, loop, filter_semantics):
+async def process_and_send_data(messages, mapping, stream, send_data, buffer_lock, loop, filter_semantics, additional_info=None):
     async with buffer_lock:
         df = pd.DataFrame(messages)
-        # logger.info("DataFrame before filtering:\n%s", df.head())
         mapped_data = mapped_values_vectorized(mapping, df)
         filtered_data = apply_filters_vectorized(mapped_data, filter_semantics)
         if not filtered_data.empty:
+            if additional_info:
+                if isinstance(additional_info, dict):
+                    stream.extras.update(additional_info)
+                elif isinstance(additional_info, str):
+                    stream.extras['additional_info'] = additional_info
+                else:
+                    stream.extras['additional_info'] = str(additional_info)
             # logger.info("Filtered DataFrame:\n%s", filtered_data.head())
             await send_data(filtered_data, stream, loop)
         else:
