f4e5a588e0a9dab10f1740c48288bbdf4fefa190
Andreufb <andreufb97@gmail.com>
2024-08-06 14:05:25 -0600

    Added S3 PUT Functioanlity (endpoint, service, model)

--- api/routes/update_routes/__init__.py
+++ api/routes/update_routes/__init__.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter
 
 from .put_kafka import router as put_kafka
 from .put_url import router as put_url
+from .put_s3 import router as put_s3
 
 router = APIRouter()
 
 router.include_router(put_kafka)
 router.include_router(put_url)
+router.include_router(put_s3)
--- api/services/s3_services/__init__.py
+++ api/services/s3_services/__init__.py
@@ -1,1 +1,2 @@
 from .add_s3 import add_s3
+from .update_s3 import update_s3
