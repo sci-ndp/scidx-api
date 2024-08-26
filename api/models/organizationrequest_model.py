135b5468f0fd94afa74f7f4b65b6f853c777d9a8
rbardaji <rbardaji@gmail.com>
2024-08-26 12:40:27 +0000

    refactor: update organizationrequest_model.py to comply with PEP8 and Pydantic v2 standards

--- api/models/organizationrequest_model.py
+++ api/models/organizationrequest_model.py
@@ -2,6 +2,24 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 class OrganizationRequest(BaseModel):
-    name: str = Field(..., json_schema_extra={"example": "example_org_name", "description": "The unique name of the organization to be created."})
-    title: str = Field(..., json_schema_extra={"example": "Example Organization Title", "description": "The title of the organization to be created."})
-    description: Optional[str] = Field(None, json_schema_extra={"example": "This is an example organization.", "description": "A description of the organization."})
+    name: str = Field(
+        ...,
+        json_schema_extra={
+            "example": "example_org_name",
+            "description": "The unique name of the organization to be created.",
+        },
+    )
+    title: str = Field(
+        ...,
+        json_schema_extra={
+            "example": "Example Organization Title",
+            "description": "The title of the organization to be created.",
+        },
+    )
+    description: Optional[str] = Field(
+        None,
+        json_schema_extra={
+            "example": "This is an example organization.",
+            "description": "A description of the organization.",
+        },
+    )
