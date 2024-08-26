946ec138e4f048fa00ae4fc7e20ef28d317388de
rbardaji <rbardaji@gmail.com>
2024-08-26 12:34:39 +0000

    refactor: update Pydantic Field usage to comply with PEP8 and Pydantic v2 standards

--- api/models/datasourcerequest_model.py
+++ api/models/datasourcerequest_model.py
@@ -2,12 +2,52 @@ from pydantic import BaseModel, Field
 from typing import Optional, Dict
 
 class DataSourceRequest(BaseModel):
-    dataset_name: str = Field(..., example="example_dataset_name", description="The unique name of the dataset to be created.")
-    dataset_title: str = Field(..., example="Example Dataset Title", description="The title of the dataset to be created.")
-    owner_org: str = Field(..., example="example_org_id", description="The ID of the organization to which the dataset belongs.")
-    resource_url: str = Field(..., example="http://example.com/resource", description="The URL of the resource to be associated with the dataset.")
-    resource_name: str = Field(..., example="Example Resource Name", description="The name of the resource to be associated with the dataset.")
-    dataset_description: str = Field("", example="This is an example dataset.", description="A description of the dataset.")
-    resource_description: str = Field("", example="This is an example resource.", description="A description of the resource.")
-    resource_format: str = Field(None, example="CSV", description="The format of the resource.")
-    extras: Optional[Dict[str, str]] = Field(None, example={"key1": "value1", "key2": "value2"}, description="Additional metadata to be added to the dataset as extras.")
+    dataset_name: str = Field(
+        ...,
+        description="The unique name of the dataset to be created.",
+        json_schema_extra={"example": "example_dataset_name"},
+    )
+    dataset_title: str = Field(
+        ...,
+        description="The title of the dataset to be created.",
+        json_schema_extra={"example": "Example Dataset Title"},
+    )
+    owner_org: str = Field(
+        ...,
+        description="The ID of the organization to which the dataset belongs.",
+        json_schema_extra={"example": "example_org_id"},
+    )
+    resource_url: str = Field(
+        ...,
+        description="The URL of the resource to be associated with the dataset.",
+        json_schema_extra={"example": "http://example.com/resource"},
+    )
+    resource_name: str = Field(
+        ...,
+        description="The name of the resource to be associated with the dataset.",
+        json_schema_extra={"example": "Example Resource Name"},
+    )
+    dataset_description: str = Field(
+        "",
+        description="A description of the dataset.",
+        json_schema_extra={"example": "This is an example dataset."},
+    )
+    resource_description: str = Field(
+        "",
+        description="A description of the resource.",
+        json_schema_extra={"example": "This is an example resource."},
+    )
+    resource_format: str = Field(
+        None,
+        description="The format of the resource.",
+        json_schema_extra={"example": "CSV"},
+    )
+    extras: Optional[Dict[str, str]] = Field(
+        None,
+        description=(
+            "Additional metadata to be added to the dataset as extras."
+        ),
+        json_schema_extra={
+            "example": {"key1": "value1", "key2": "value2"}
+        },
+    )
