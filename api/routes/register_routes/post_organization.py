598f0cbffb7612c6094ff07b64a7c006d93584c8
rbardaji <rbardaji@gmail.com>
2024-08-26 22:28:34 +0000

    style: refactor create_organization_endpoint to comply with PEP 8 standards

--- api/routes/register_routes/post_organization.py
+++ api/routes/register_routes/post_organization.py
@@ -3,7 +3,6 @@ from api.models import OrganizationRequest
 from api.services import organization_services
 from tenacity import RetryError
 from typing import Dict, Any
-
 from api.services.keycloak_services.get_current_user import get_current_user
 
 router = APIRouter()
@@ -13,7 +12,10 @@ router = APIRouter()
     response_model=dict,
     status_code=status.HTTP_201_CREATED,
     summary="Create a new organization",
-    description="Create a new organization with the given name, title, and description.",
+    description=(
+        "Create a new organization with the given name, title, "
+        "and description."
+    ),
     responses={
         201: {
             "description": "Organization created successfully",
@@ -21,7 +23,7 @@ router = APIRouter()
                 "application/json": {
                     "example": {
                         "id": "305284e6-6338-4e13-b39b-e6efe9f1c45a",
-                        "message": "Organization created successfully"
+                        "message": "Organization created successfully",
                     }
                 }
             }
@@ -40,42 +42,61 @@ router = APIRouter()
 )
 async def create_organization_endpoint(
     org: OrganizationRequest,
-    _: Dict[str, Any] = Depends(get_current_user)):
+    _: Dict[str, Any] = Depends(get_current_user)
+):
     """
     Endpoint to create a new organization in CKAN.
 
     Parameters
     ----------
     org : OrganizationRequest
-        An object containing the name, title, and description of the organization.
+        An object containing the name, title, and description of 
+        the organization.
 
     Returns
     -------
     dict
-        A dictionary containing the ID of the created organization and a success message.
+        A dictionary containing the ID of the created organization 
+        and a success message.
 
     Raises
     ------
     HTTPException
-        If there is an error creating the organization, an HTTPException is raised with a detailed message.
+        If there is an error creating the organization, an 
+        HTTPException is raised with a detailed message.
     """
     try:
         organization_id = organization_services.create_organization(
             name=org.name,
             title=org.title,
-            description=org.description
+            description=org.description,
         )
-        return {"id": organization_id, "message": "Organization created successfully"}
+        return {
+            "id": organization_id,
+            "message": "Organization created successfully",
+        }
     except RetryError as e:
         final_exception = e.last_attempt.exception()
         error_message = str(final_exception)
         if "Group name already exists in database" in error_message:
-            raise HTTPException(status_code=400, detail="Organization name already exists.")
+            raise HTTPException(
+                status_code=400, 
+                detail="Organization name already exists."
+            )
         elif "Validation error" in error_message:
-            raise HTTPException(status_code=400, detail=f"Validation error: {error_message}")
+            raise HTTPException(
+                status_code=400, 
+                detail=f"Validation error: {error_message}"
+            )
         elif "CKAN API endpoint not found" in error_message:
-            raise HTTPException(status_code=404, detail="CKAN API endpoint not found")
+            raise HTTPException(
+                status_code=404, 
+                detail="CKAN API endpoint not found"
+            )
         else:
-            raise HTTPException(status_code=400, detail=f"Unexpected error: {error_message}")
+            raise HTTPException(
+                status_code=400, 
+                detail=f"Unexpected error: {error_message}"
+            )
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
