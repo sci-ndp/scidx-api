dd128f8eb097901d0314b2f9f846ba3956c9d348
rbardaji <rbardaji@gmail.com>
2024-08-30 10:13:59 +0000

    feat: Add POP setting to swagger_settings.py

--- api/config/swagger_settings.py
+++ api/config/swagger_settings.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     swagger_title: str = "API Documentation"
     swagger_description: str = "This is the API documentation."
     swagger_version: str = "0.1.0"
+    pop: bool = False
 
     model_config = {
         "env_file": "./env_variables/.env_swagger",
